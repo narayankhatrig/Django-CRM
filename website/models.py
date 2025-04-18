1bcde670e2b84b55e068fdf3cadb957a8afd48f0
Narayan <narayankhatrig@gmail.com>
2025-04-19 02:46:27 +0900

    user registration and database model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class UserInformation(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email_address = models.CharField(max_length=100)
+    phone_no = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=50)
+
+    def __str__(self):
+        return(f"{self.firstname} {self.lastname}")
--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User, auth
+from django.contrib.auth import authenticate
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -11,9 +13,9 @@ def home(request):
         password = request.POST['password']
 
         #authenticate the user
-        user = authenticate(request, username=username, password=password)
+        user = auth.authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user)
+            auth.login(request, user)
             messages.success(request, "You Have Been Logged In!")
             return redirect('home')
         else:
@@ -24,10 +26,36 @@ def home(request):
 
 
 '''def login_user(request):
-    pass
+    return render(request, 'home.html')
 '''
 
+@login_required(login_url='home')
 def logout_user(request):
-    logout(request)
+    auth.logout(request)
     messages.success(request, "You Have Been Logged Out!")
     return redirect('home')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.success(request, "Email Already Taken")
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.success(request, "Username Already Taken")
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, password=password, email=email)
+                auth.login(request, user)
+                messages.success(request, "You Have Successfully Registered...")
+                return redirect('home')
+        else:
+            messages.success(request, "Password Do Not Match!")
+    else:
+        return render(request, 'signup.html')
+    
